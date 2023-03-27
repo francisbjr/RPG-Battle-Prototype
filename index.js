a2f599a0c0dad5388a1f3ab88a27a98bbeb7f323
JunBug <8799137+francisbjr@users.noreply.github.com>
2023-03-27 19:39:25 -0400

    Add images for map and player
    Replaced demo map image w/ bigger file, added player images, centered map and player images.

--- index.js
+++ index.js
@@ -7,3 +7,14 @@ canvas.height = 576
 
 c.fillStyle = 'white'
 c.fillRect(0,0, canvas.width, canvas.height)
+
+const playerImage = new Image()
+playerImage.src = './images/playerDown.png'
+
+const map = new Image()
+map.src = './images/Demo Town.png'
+
+map.onload = () => {
+    c.drawImage(map, -832, -600)
+    c.drawImage(playerImage, canvas.width / 2 - playerImage.width / 2, canvas.height / 2)
+}
